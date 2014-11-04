bd1ca713f7f4a7ccd88f810d5d4fba507f1b9282
Kivanc Yazan <kivancyazan@gmail.com>
2014-11-04 21:43:39 +0200

    profile jsp

---
+++ Web/src/Profile.java
@@ -0,0 +1,61 @@
+
+
+import java.io.IOException;
+import java.util.Calendar;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Profile
+ */
+@WebServlet("/Profile")
+public class Profile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Profile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html"); 
+		User user = new User();
+		user.setName(request.getParameter("name"));
+		user.setPassword(request.getParameter("password"));
+		user.setEmail(request.getParameter("email"));
+		Calendar cal = Calendar.getInstance();
+		java.sql.Timestamp timestamp = new java.sql.Timestamp(cal.getTimeInMillis());
+		user.setIsDeleted(0);
+		user.setCreatedOn(timestamp);
+		user.setUpdatedOn(timestamp);
+		Boolean result = user.Register();
+		
+			HttpSession session = request.getSession();
+			session.setAttribute("loggedIn", true);
+			session.setAttribute("email", request.getParameter("email"));
+			if(result)
+			{
+				response.getWriter().write("true");
+			}else
+				response.getWriter().write("false");
+
+	}
+}
